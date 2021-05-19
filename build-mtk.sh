9098b6f6ef1dc631977376c53453d907ff60b04b
dtltljb <ljb@lqtech.com>
2021-05-19 12:25:07 +0800

    create repository

---
+++ build-mtk.sh
@@ -0,0 +1,3 @@
+
+export PATH=/opt/ixecloud-toolchain-0.11-ramips-mt7621_gcc-7.5.0_musl.Linux-x86_64/toolchain-mipsel_24kc_gcc-7.5.0_musl/bin:$PATH
+export STAGING_DIR=/home/robot/OpenWrt/mtk7621-19.07/staging_dir
